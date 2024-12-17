960c609527df45e6302bad30e4e4d16ef228f82d
StuPidMRE1107 <xie15978359260@163.com>
2024-12-17 10:43:35 +0100

    Finish code for RL.

---
+++ rl/rl5/student_5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Q learning learning rate
+alpha = 0.3
+
+# Q learning discount rate
+gamma = 0.5
+
+# Epsilon initial
+epsilon_initial = 1
+
+# Epsilon final
+epsilon_final = 0.
+
+# Annealing timesteps
+annealing_timesteps = 1000
+
+# threshold
+threshold = 1e-6
